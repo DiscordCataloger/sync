42b63bc946c50c8b228e04e7c9dee52a910adc5c
danielng2024 <ngchung2016@gmail.com>
2024-07-17 09:19:48 +0800

    first upload

---
+++ src/app/(frontend)/(components)/sidebar.js
@@ -0,0 +1,32 @@
+import { AiFillMessage } from "react-icons/ai";
+import { FaComments, FaPinterest, FaLinkedin, FaPlusCircle, FaBell } from 'react-icons/fa';
+import styles from "./components/Sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles.button}>
+        <AiFillMessage size={40} className={styles.icon} />
+      </div>
+      <div className={styles.button}>
+        <FaPinterest size={40} className={styles.icon} />
+      </div>
+      <div className={styles.button}>
+        <FaLinkedin size={40} className={styles.icon} />
+      </div>
+      <div className={styles.button}>
+        <FaPlusCircle size={40} className={styles.icon} />
+      </div>
+
+      <div className={styles.notification}>
+        <FaBell size={40} className={styles.icon} />
+      </div>
+      <div className={styles.profile}>
+        <Image src="/profile.png" alt="Profile" width={40} height={40} className={styles.profileImage} />
+        <div className={styles.status}></div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;

0e62a84e3665f53903d4d228cbe5cdd4bf555951
le,;wlc <jckjdkdmd@gmail.com>
2024-07-18 13:39:59 +0800

    merging both Jonathan_register and Jonathan_login

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,15 +1,27 @@
+<<<<<<< HEAD
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
+=======
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
+import { cva } from "class-variance-authority";
+
+import { cn } from "@/lib/utils"
+>>>>>>> 1b04978 (adding shadcn)
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
+<<<<<<< HEAD
         default: "bg-primary text-primary-foreground hover:bg-primary/80",
+=======
+        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+>>>>>>> 1b04978 (adding shadcn)
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
@@ -31,6 +43,7 @@ const buttonVariants = cva(
       size: "default",
     },
   }
+<<<<<<< HEAD
 );
 
 const Button = React.forwardRef(
@@ -48,3 +61,19 @@ const Button = React.forwardRef(
 Button.displayName = "Button";
 
 export { Button, buttonVariants };
+=======
+)
+
+const Button = React.forwardRef(({ className, variant, size, asChild = false, ...props }, ref) => {
+  const Comp = asChild ? Slot : "button"
+  return (
+    (<Comp
+      className={cn(buttonVariants({ variant, size, className }))}
+      ref={ref}
+      {...props} />)
+  );
+})
+Button.displayName = "Button"
+
+export { Button, buttonVariants }
+>>>>>>> 1b04978 (adding shadcn)

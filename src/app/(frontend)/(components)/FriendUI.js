dcd231c18564d6e645bddb6238a4c11b1302b790
DanielHsuGou <goutanuki513@gmail.com>
2024-07-22 15:45:20 +0800

    Modified TemplateUI for FriendUI

--- src/app/(frontend)/(components)/FriendUI.js
+++ src/app/(frontend)/(components)/FriendUI.js
@@ -1,5 +1,5 @@
 "use client";
-import Topbar from "./topbar";
+import TemplateUI from "./TemplateUI";
 import { Github } from "lucide-react";
 
 export default function FriendUI() {
@@ -8,7 +8,7 @@ export default function FriendUI() {
 
   return (
     <div className="p-5  min-w-[500px]">
-      <Topbar icon={<Github size="30" />} name={name} category={category} />
+      <TemplateUI icon={<Github size="30" />} name={name} category={category} />
     </div>
   );
 }

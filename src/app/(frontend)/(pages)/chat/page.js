cf6961fe2862c3a8bb8a30c0c972c270c4dc98c0
DanielHsuGou <goutanuki513@gmail.com>
2024-07-23 10:02:46 +0800

    override with local folder

---
+++ src/app/(frontend)/(pages)/chat/page.js
@@ -0,0 +1,10 @@
+"use client";
+import FriendUI from "../../(components)/FriendUI";
+
+export default function Page() {
+  return (
+    <div className="bg-blue-100 h-screen">
+      <FriendUI />
+    </div>
+  );
+}

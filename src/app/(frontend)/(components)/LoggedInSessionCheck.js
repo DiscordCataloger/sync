adbbcb5994b86c4b84aa948aaf95d93ef231aa04
Jonathan Tsai <cat2006house@gmail.com>
2024-08-06 16:25:44 +0800

    fix

--- src/app/(frontend)/(components)/LoggedInSessionCheck.js
+++ src/app/(frontend)/(components)/LoggedInSessionCheck.js
@@ -1,35 +1,35 @@
-"use client";
-import { useRouter } from "next/navigation";
-import Cookies from "js-cookie";
-import { useEffect } from "react";
-import { getSession } from "next-auth/react";
+// "use client";
+// import { useRouter } from "next/navigation";
+// import Cookies from "js-cookie";
+// import { useEffect } from "react";
+// import { getSession } from "next-auth/react";
 
-export default function LoggedInSessionCheck() {
-  const router = useRouter();
+// export default function LoggedInSessionCheck() {
+//   const router = useRouter();
 
-  useEffect(() => {
-    async function checkSession() {
-      const session = await getSession(); // Ensure this is awaited
-      const rememberMe = Cookies.get("rememberMe");
-      // console.log("Session:", session); // Debugging log
-      // console.log("rememberMe cookie:", rememberMe); // Debugging log
+//   useEffect(() => {
+//     async function checkSession() {
+//       const session = await getSession(); // Ensure this is awaited
+//       const rememberMe = Cookies.get("rememberMe");
+//       // console.log("Session:", session); // Debugging log
+//       // console.log("rememberMe cookie:", rememberMe); // Debugging log
 
-      if (session) {
-        if (rememberMe) {
-          console.log("Redirecting to /chat");
-          router.push("/chat");
-        } else {
-          console.log("Session exists but rememberMe is not set");
-          // Handle the case where session exists but rememberMe is not set
-        }
-      } else {
-        console.log("No session found, redirecting to login");
-        router.push("/login");
-      }
-    }
+//       if (session) {
+//         if (rememberMe) {
+//           console.log("Redirecting to /chat");
+//           router.push("/chat");
+//         } else {
+//           console.log("Session exists but rememberMe is not set");
+//           // Handle the case where session exists but rememberMe is not set
+//         }
+//       } else {
+//         console.log("No session found, redirecting to login");
+//         router.push("/login");
+//       }
+//     }
 
-    checkSession();
-  }, [router]);
+//     checkSession();
+//   }, [router]);
 
-  return null;
-}
+//   return null;
+// }

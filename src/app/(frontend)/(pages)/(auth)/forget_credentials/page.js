b7eab480131c8b8bd8093f6740b128c16c142cbc
Jonathan Tsai <cat2006house@gmail.com>
2024-08-06 16:31:07 +0800

    what

--- src/app/(frontend)/(pages)/(auth)/forget_credentials/page.js
+++ src/app/(frontend)/(pages)/(auth)/forget_credentials/page.js
@@ -2,7 +2,7 @@ import ForgetPageComponent from "@/app/(frontend)/(components)/forget";
 import SlideProvider from "../(slide)/slideProvider";
 // import LoggedInSessionCheck from "@/app/(frontend)/(components)/LoggedInSessionCheck";
 
-// export const metadata = {
+export const metadata = {
   title: "Account Recovery",
   description: "Account Recovery on Sync",
 };
